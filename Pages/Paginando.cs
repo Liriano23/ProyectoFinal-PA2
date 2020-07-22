8e72e4c3269d11e29074788493a7cfe2df2d4dde
Juan De Dios Fernandez Camilo <juan_fernandez3@ucne.edu.do>
2020-07-21 23:12:08 -0400

    se modifico el menu pricipal, se agregaron las consultas de categorias, compras, empleados y suplidores, tambien se le añadio la paginacion a todos las paginas de las consultas

---
+++ Pages/Paginando.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_PA2.Pages
+{
+    public class Paginando
+    {
+        public int Pagina { get; set; } = 1;
+        public int CantidadMostrar { get; set; } = 5;
+    }
+}
